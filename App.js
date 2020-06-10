e74f1e27d1cce28d68f85d345d3913b19c8dd3b9
nicolasarcas <nicolas.arcas1@gmail.com>
2020-06-10 16:00:18 -0300

    Primeiro exercicio

--- App.js
+++ App.js
@@ -1,19 +1,95 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
 
 export default function App() {
+
+  const[numero,setNumero] = useState('');
+  const[nome,setNome] = useState('');
+  const[contatos,setContatos] = useState([]);
+  const[contadorNomes,setContadorNomes] = useState(0);
+  const[contadorNumeros,setContadorNumeros] = useState(1000);
+
+  const capturarNumero = (numero)=>{
+    setNumero(numero);
+  }
+
+  const capturarNome = (nome)=>{
+    setNome(nome);
+  }
+
+  const adicionarContato = () =>{
+    setContatos((contatos)=>{
+      setContadorNomes(contadorNomes+1);
+      setContadorNumeros(contadorNumeros+1);
+      return[{key: contadorNomes.toString(),value:nome},{key: contadorNumeros.toString(),value:numero},...contatos]
+    });
+    setNome('');
+    setNumero('');
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.telaPrincipalView}>
+      <View>
+        <Text>Novo contato</Text>
+        <TextInput style={styles.estiloTextInput}
+          placeholder='Nome'
+          value={nome}
+          onChangeText={capturarNome}
+        />
+        <TextInput style={styles.estiloTextInput}
+          placeholder='Número'
+          value = {numero}
+          onChangeText={capturarNumero}
+        />
+        <Button
+          title='Adicionar'
+          onPress={adicionarContato}
+        />
+      </View>
+        <Text>Contatos</Text>
+      <View style={styles.lembreteView}>
+        <FlatList
+          data={contatos}
+          renderItem={
+            nome =>(
+              <View style={styles.itemNaLista}>
+                  <Text>{nome.item.value}</Text>
+              </View>
+            ),
+            numero =>(
+              <View style={styles.itemNaLista}>
+                  <Text>{numero.item.value}</Text>
+              </View>
+            )          
+          }
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  estiloTextInput: {
+    width: '80%',
+    borderBottomColor: 'black',
+    borderBottomWidth: 2,
+    marginBottom: 4,
+    padding: 4
   },
+  telaPrincipalView: {
+    padding: 50
+  },
+  itemNaLista: {
+    padding: 12,
+    backgroundColor: '#EEE',
+    borderColor: '#CCC',
+    borderWidth: 1,
+    marginBottom: 8,
+    borderRadius: 8,
+  },
+  lembreteView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  }
 });
